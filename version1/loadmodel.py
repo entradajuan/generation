32f71da90e3ef0c32305af61941cd282b43b3b49
entradajuan <entradajuan@yahoo.es>
2021-09-01 11:02:43 +0000

    choosing the model

--- version1/loadmodel.py
+++ version1/loadmodel.py
@@ -37,6 +37,17 @@ def build_gen_model(vocab_size, embedding_dim, rnn_units, batch_size):
   ])
   return model
 
+def build_gen_model(vocab_size, embedding_dim, rnn_units, batch_size):
+  model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(vocab_size, embedding_dim,
+                              batch_input_shape=[batch_size, None]),
+    tf.keras.layers.GRU(rnn_units,
+                        return_sequences=True,
+                        stateful=True,
+                        recurrent_initializer='glorot_uniform'),
+    tf.keras.layers.Dense(vocab_size)
+  ])
+  return model
 
 def build_gen_model2(vocab_size, embedding_dim, rnn_units, batch_size):
   model = tf.keras.Sequential([tf.keras.layers.Embedding(vocab_size, embedding_dim, mask_zero=True, input_length=99, batch_size= batch_size),
@@ -51,48 +62,38 @@ embedding_dim = 256
 rnn_units = 1024
 batch_size = 1 #128
 
-gen_model = build_gen_model2(vocab_size, embedding_dim, rnn_units, batch_size)
+gen_model = build_gen_model(vocab_size, embedding_dim, rnn_units, batch_size)
 
-checkpoint_dir = './version1/training_checkpoints/2021-Aug-28-18-25-45/' 
+checkpoint_dir = './training_checkpoints/2021-Sep-01-09-22-12/' 
 
 gen_model.load_weights(tf.train.latest_checkpoint(checkpoint_dir))
 
 gen_model.build(tf.TensorShape([1, None]))
 
 
-def generate_text(model, start_string, temperature=0.7, num_generate=99):
-  # Low temperatures results in more predictable text.
-  # Higher temperatures results in more surprising text.
-  # Experiment to find the best setting.
-
-  # Converting our start string to numbers (vectorizing)
+def generate_text(model, start_string, temperature=0.2, num_generate=99):
   input_eval = [char2idx[s] for s in start_string]
   input_eval = tf.expand_dims(input_eval, 0)
 
-  # Empty string to store our results
   text_generated = []
 
   # Here batch size == 1
   for i in range(num_generate):
       predictions = model(input_eval)
-      # remove the batch dimension
+
       predictions = tf.squeeze(predictions, 0)
 
-      # using a categorical distribution to predict the word returned by the model
+
       predictions = predictions / temperature
       predicted_id = tf.random.categorical(predictions, num_samples=1)[-1,0].numpy()
 
-      # We pass the predicted word as the next input to the model
-      # along with the previous hidden state
+
       input_eval = tf.expand_dims([predicted_id], 0)
         
       text_generated.append(idx2char[predicted_id])
-      # lets break is <EOS> token is generated
-      #if idx2char[predicted_id] == EOS:
-      #  break #end of a sentence reached, lets stop
+
 
   return (start_string + ''.join(text_generated))
 
-print(generate_text(gen_model, start_string=u"Seattle"))
+print(generate_text(gen_model, start_string=u"Sevilla"))
   
-
